948ea421c87033c14b4089a0693ed547557f350f
Pradeep Shukla <pradeepshukla621@gmail.com>
2024-12-10 11:16:51 +0530

    'backup'

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+    {
+        path:'layout', loadChildren:() => import('./layout/layout.module').then(m => m.LayoutModule)
+    }
+];
