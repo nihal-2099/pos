0c9817aa645ba94237d8fa7de28586924f2952a6
RitinSupplyValid <rnijhawan@supplyvalid.com>
2024-12-11 11:37:46 +0530

    layout setting

---
+++ src/app/layout/add-user/add/add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  standalone: false,
+  
+  templateUrl: './add.component.html',
+  styleUrl: './add.component.scss'
+})
+export class AddComponent {
+
+}
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
-import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ServicesComponent } from './services/services.component';
+
 
 const routes: Routes = [
 
   {
     path: '', component: LayoutComponent, children: [
 
-      { path: 'home', component: HomeComponent },
-      {path:'aboutus', component:AboutUsComponent},
-      {path:'services', component:ServicesComponent}
+      {  path:'add',  loadChildren:() => import('./add-user/add-user.module').then(m => m.AddUserModule)}
+      // { path: 'home', component: HomeComponent },
+      // {path:'aboutus', component:AboutUsComponent},
+      // {path:'services', component:ServicesComponent}
     ]
   }
 ];
