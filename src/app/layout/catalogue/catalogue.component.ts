24337643613685b1978e27a3440dcad4b2e877ee
RitinSupplyValid <rnijhawan@supplyvalid.com>
2024-12-12 14:57:32 +0530

    side bar changes

---
+++ src/app/layout/catalogue/catalogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue',
+  standalone: false,
+  
+  templateUrl: './catalogue.component.html',
+  styleUrl: './catalogue.component.scss'
+})
+export class CatalogueComponent {
+
+}
