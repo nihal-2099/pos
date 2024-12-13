58ce50bffb6347f8b676459a749d9a2f8c7f6762
RitinSupplyValid <rnijhawan@supplyvalid.com>
2024-12-13 17:33:07 +0530

    side bar fixed

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -9,15 +9,21 @@ import { ShowHideBarService } from '../services/show-hide-bar.service';
 })
 export class LayoutComponent {
   isShow:boolean = true
+  isSide:boolean = true
   constructor (public showHideBarService:ShowHideBarService){
     // Subscribe to the shared message
     this.showHideBarService.updateClose.subscribe(
       (message) => this.isShow = message
     );
+
+    this.showHideBarService.updateSideClose.subscribe(
+      (sideupdate) => this.isSide = sideupdate
+    )
 }
 
 ngOnInit() {
   // Subscribe to the message
- this.isShow =  this.showHideBarService. getCurrentValue()
+ this.isShow = this.showHideBarService.getCurrentValue()
+ this.isSide = this.showHideBarService.getSideCurrentValue()
 }
 }
