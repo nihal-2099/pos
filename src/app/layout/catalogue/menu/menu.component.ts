4010488810bbaee3d6334e1fc7d9b83dcaf3b8d3
Pradeep Shukla <psuklasupplyvalid.com>
2024-12-18 12:16:10 +0530

    validation update in menucomponent

--- src/app/layout/catalogue/menu/menu.component.ts
+++ src/app/layout/catalogue/menu/menu.component.ts
@@ -23,14 +23,14 @@ export class MenuComponent {
       displayOrder: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
       preparationAs: new FormControl ('', [Validators.required]),
       hsnNo: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
-      taxType: new FormControl ( [false]), // For checkboxes, you can use a boolean
+      taxType: new FormControl ( false), // For checkboxes, you can use a boolean
       kitchenInstructions: new FormControl ('', [ Validators.required]),
-      status: new FormControl ( [false]), // For checkboxes, you can use a boolean
+      status: new FormControl ( false), // For checkboxes, you can use a boolean
       minQty: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
       maxQty: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
-      inventoryApplicable: new FormControl ( [false]),
-      batchApplicable: new FormControl ( [false]),
-      updateChangeInCost: new FormControl ( [false]),
+      inventoryApplicable: new FormControl ( false),
+      batchApplicable: new FormControl ( false),
+      updateChangeInCost: new FormControl ( false),
       decimalsForQty: new FormControl ('', [ Validators.required]),
       abandonPercentage: new FormControl ('', [Validators.required]),
       discountType: new FormControl ('', [Validators.required]),
@@ -40,19 +40,38 @@ export class MenuComponent {
       discount: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
       packingAmount: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
       sku: new FormControl ('', [Validators.required]),
-      applyForOnline: new FormControl ( [false]),
+      applyForOnline: new FormControl ( false),
 
-      extraToppings: new FormControl ( [false]),
-      extraCream: new FormControl ( [false]),
-      greenSalad: new FormControl ( [false])
+      regularcostPrice: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      regularsellingPrice: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      regulardiscount: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      regularpackingAmount: new FormControl ('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+      regularsku: new FormControl ('', [Validators.required]),
+      regularapplyForOnline: new FormControl (false),
+
+      extraToppings: new FormControl (false),
+      extraCream: new FormControl ( false),
+      greenSalad: new FormControl ( false)
   });
 
 
   formValue: any;
 
-  onSave() {
-    console.log(this.menucomponentform)
-       this.formValue = this.menucomponentform.value;
+   // Method to handle form submission
+   submitForm(type:any): void {
+    console.log(type)
+    if(type == 'save'){
+      if (this.menucomponentform.invalid) {
+        alert('Please fill all required fields correctly.');
+        this.menucomponentform.markAllAsTouched()
+
+      } else {
+        // Handle form submission logic here
+        alert('Form submitted successfully!');
+      }
+
+    }
+ 
   }
 
 }
